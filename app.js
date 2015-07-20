297dd7bbed8d8efcfa1432361424c21c7831923f
Dion Yang <jellopops@gmail.com>
2015-07-19 18:23:49 -0700

    added jsonp callback

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 var app = angular.module('plunker', []);
 
-app.controller('MainCtrl', function($scope) {
-  $scope.submit = function () { 
-  	console.log($scope.search);
-  };
+app.controller('MainCtrl', function($scope, $http, $q) {
+	$scope.searchQuery = '';
+	$scope.searchQueryAmt = 0;
+
+	$scope.submit = function () { 
+		$scope.searchQuery = $scope.search;
+		$scope.search = '';
+		$scope.doSearch();
+		console.log($scope.searchQuery);
+	};
+
+	$scope.doSearch = function () {
+		var url = "https://api.instagram.com/v1/tags/" + $scope.searchQuery + "/media/recent",
+			request = {
+				callback: 'JSON_CALLBACK',
+				client_id: 'e4cf3dcd43bc45e8a54771b2a2f13b32'
+			};
+		$http({
+			method: 'JSONP',
+			url: url,
+			params: request
+		})
+		.success(function (result) {
+			$scope.dataSet = result.data;
+			$scope.searchQueryAmt = $scope.dataSet.length;
+			console.log($scope.dataSet);
+		})
+		.error(function () {
+			console.log('error');
+		});
+	};
 });
